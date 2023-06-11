1afe396d1151cf40af4188ae6cfbc89fd544f256
PhamTruongTrinh <122708241+PhamTruongTrinh@users.noreply.github.com>
2023-06-11 23:53:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 #default page of our web-app
 @app.route()home()
 def home():
-    return render_template('https://phamtruongtrinh.github.io/dubaoratruongdunghan/')
+    return render_template('https://phamtruongtrinh.github.io/dubaoratruongdunghan/predict')
 #To use the predict button in our web-app
 @app.route('https://phamtruongtrinh.github.io/dubaoratruongdunghan/',methods=['POST'])
 def predict():
@@ -24,9 +24,9 @@ def predict():
     output = round(prediction[0])
     int(output)
     if output == 1:
-        return render_template('https://phamtruongtrinh.github.io/dubaoratruongdunghan/', prediction_text='Chúc mừng rất có thể bạn sẽ ra trường đúng hạn')
+        return render_template('index.html', prediction_text='Chúc mừng rất có thể bạn sẽ ra trường đúng hạn')
     if output == 0:
-        return render_template('https://phamtruongtrinh.github.io/dubaoratruongdunghan/', prediction_text='Có thể bạn sẽ ra trường không đúng hạn. Hãy cố gắng hơn trong học tập')
+        return render_template('index.html', prediction_text='Có thể bạn sẽ ra trường không đúng hạn. Hãy cố gắng hơn trong học tập')
 
 if __name__ == "__main__":
     app.run(debug=True)
